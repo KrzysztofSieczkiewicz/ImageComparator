fe07209e7f3df8181a296fb987103490d24876a3
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-12-29 14:57:01 +0100

    change MismatchesGroup to a record class

--- src/main/java/org/example/analyzers/MismatchesGroup.java
+++ src/main/java/org/example/analyzers/MismatchesGroup.java
@@ -1,45 +1,10 @@
 package org.example.analyzers;
 
-import java.awt.*;
+import java.awt.Rectangle;
 
-public class MismatchesGroup {
-    private final int size;
-
-    private final int minX;
-    private final int maxX;
-    private final int minY;
-    private final int maxY;
-
-    public MismatchesGroup(int size, int minX, int maxX, int minY, int maxY) {
-        this.size = size;
-
-        this.minX = minX;
-        this.maxX = maxX;
-        this.minY = minY;
-        this.maxY = maxY;
-    }
+public record MismatchesGroup(int size, int minX, int maxX, int minY, int maxY) {
 
     public Rectangle getBoundingRectangle() {
         return new Rectangle(minX, minY, maxX - minX, maxY - minY);
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public int getMinX() {
-        return minX;
-    }
-
-    public int getMaxX() {
-        return maxX;
-    }
-
-    public int getMinY() {
-        return minY;
-    }
-
-    public int getMaxY() {
-        return maxY;
     }
 }
