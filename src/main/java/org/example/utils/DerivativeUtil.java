67e0ce93f0388f6c9ae876dbb74a6bf04758652f
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-02-03 14:37:58 +0100

    debugging, updated dxy sobel kernel

--- src/main/java/org/example/utils/DerivativeUtil.java
+++ src/main/java/org/example/utils/DerivativeUtil.java
@@ -27,9 +27,9 @@ public class DerivativeUtil {
     };
 
     private static int[][] sobelDxy = {
-            {-1,  0,  1},
-            { 0,  0,  0},
-            { 1,  0, -1}
+            {  1, -2,  1},
+            { -2,  4, -2},
+            {  1, -2,  1}
     };
 
     /**
