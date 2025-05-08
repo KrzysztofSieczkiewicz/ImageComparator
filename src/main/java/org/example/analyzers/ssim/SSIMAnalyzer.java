e338fe42cdc66b5ffc2a3e7af0998d63b9502594
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-05-08 22:15:33 +0200

    luminosity component wip

--- src/main/java/org/example/analyzers/ssim/SSIMAnalyzer.java
+++ src/main/java/org/example/analyzers/ssim/SSIMAnalyzer.java
@@ -6,26 +6,35 @@ import java.awt.image.BufferedImage;
 
 public class SSIMAnalyzer {
 
-    public void slideWindow(BufferedImage image, int windowSize) {
-        ImageAccessor imageAccessor = ImageAccessor.create(image);
-        int[][] imageData = imageAccessor.getPixels();
-        int imageWidth = image.getWidth();
-        int imageHeight = image.getHeight();
+    public void slideWindow(BufferedImage firstImage, BufferedImage secondImage, int windowDimension) {
+        ImageAccessor firstAccessor = ImageAccessor.create(firstImage);
+        ImageAccessor secondAccessor = ImageAccessor.create(secondImage);
+        int[][] firstImageData = firstAccessor.getPixels();
+        int[][] secondImageData = secondAccessor.getPixels();
+        int imageWidth = firstAccessor.getWidth();
+        int imageHeight = firstAccessor.getHeight();
+        int windowSize = windowDimension * windowDimension;
 
-        int windowLuminosity = 0;
+        double sigma = 0.001;
 
         for (int x=0; x<imageWidth-windowSize; x++) {
             for (int y=0; y<imageHeight-windowSize; y++) {
-                windowLuminosity = sumWindowValues(imageData, x, y, windowSize);
+                int sumLuminosity1 = sumWindowValues(firstImageData, x, y, windowDimension);
+                double meanLuminosity1 = (double) sumLuminosity1 /windowSize;
+
+                int sumLuminosity2 = sumWindowValues(secondImageData, x, y, windowDimension);
+                double meanLuminosity2 = (double) sumLuminosity2 /windowSize;
+
+                double luminosityComponent = (2 * meanLuminosity1 * meanLuminosity2 + sigma) / (meanLuminosity1*meanLuminosity1 + meanLuminosity2*meanLuminosity2 + sigma);
             }
         }
     }
 
-    public int sumWindowValues(int[][] imageData, int startX, int startY, int windowSize) {
+    public int sumWindowValues(int[][] imageData, int startX, int startY, int windowDimension) {
         int sum = 0;
 
-        for (int x=0; x<windowSize; x++) {
-            for (int y=0; y<windowSize; y++) {
+        for (int x=0; x<windowDimension; x++) {
+            for (int y=0; y<windowDimension; y++) {
                 sum += imageData[startX+x][startY+y];
             }
         }
